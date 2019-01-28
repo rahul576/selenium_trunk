b26548ff1a83c3e2a811eed63e6eaa592c169c75
rahuls <rahul.007.shrivastava@gmail.com>
2019-01-29 01:35:27 +0530

    Added the pom dependencies for selenium and testNG

--- seleniumfw/src/test/java/com/selenium999/seleniumfw/AppTest.java
+++ seleniumfw/src/test/java/com/selenium999/seleniumfw/AppTest.java
@@ -1,38 +1,12 @@
 package com.selenium999.seleniumfw;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestNG
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+   
 }
